0e8ea2978c56f54e61fc4d9e9d5c27be818cf9b1
이소윤 <s_lee77176@fanshaweonline.ca>
2020-01-14 16:52:50 -0500

    nav bar with vue

--- js/main.js
+++ js/main.js
@@ -1,25 +1,44 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-const myVM = (() => {
-    let vue_vm = new Vue({
-        data: {
-            message: "Hello from Vue!",
+var vm = new Vue({
+  el: "#app",
 
-            collection: [
-                { name: "Soyoon", role: "Student"},
-                { name: "Trevor", role: "Student"},
-                { name: "hehehe", role: "hohoho"}
-            ]
-        },
+  data: {
+    // mock up the user - this well eventually come from the database UMS (user management system)
 
-        methods: {
-            logClicked(){
-                console.log('click on an ele!');
-                
+    user: {
+        isAdmin: true,
+        isLoggedIn: true,
+        avatar: "thor.png"
+    },
 
-            }
 
-        }
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
 
-    }).$mount("#app");
-})();
+    showDetails: false
+  },
+
+  methods: {
+    logInOut() {
+        //test the login/logout UI -> button should change color
+        //eventually we'll use routing and a login component
+        console.log('do login/logout on Click');
+
+        // ? : is a ternary statement (shorthand for if/ else)
+        // evaluate the expression; if it's true, use the value to the left of the colon, 
+        //if it's false, use the value to the right
+
+        this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    },
+
+    setUserPrefs() {
+        console.log('set user prefs via routing and probabaly a component');
+    }
+
+  }
+});
